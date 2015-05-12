2a761098fe49c1ef0809cd6dfc1c121dfba45d7c
Lisyk <sergey.liskovoy@gmail.com>
2015-05-11 20:45:39 -0400

    add db, models and associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
-  belongs_to :user, foreign_key: :user_id
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+  # in case to use correct class name - belongs_to :user
+  has_many :comments
 end

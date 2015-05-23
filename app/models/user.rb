f9635636f33f6df5138e49f69e29b87dee0a162e
Lisyk <sergey.liskovoy@gmail.com>
2015-05-22 20:49:27 -0400

    set up a polymorphic association with Post and Comments models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+  has_many :votes
 
   has_secure_password validations: false
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create
+
+
 end

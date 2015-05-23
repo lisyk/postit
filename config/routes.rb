e6f85f5b50692c7457bd56dcaa184963943e0b2f
Lisyk <sergey.liskovoy@gmail.com>
2015-05-22 23:19:11 -0400

    implement votes for the posts

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ PostitTemplate::Application.routes.draw do
 
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
+    member do
+      post 'vote'
+    end
   end
 
   resources :categories, only: [:new, :create, :show]

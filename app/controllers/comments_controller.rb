9617d642e4320102417a23bd5cac71daff53ac6e
Lisyk <sergey.liskovoy@gmail.com>
2015-05-26 16:36:50 -0400

    add ajax for comments voting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,13 +16,19 @@ class CommentsController < ApplicationController
   end
 
   def vote
-    comment = Comment.find(params[:id])
-    vote = Vote.create(voteable: comment, creator: current_user, vote: params[:vote])
-    if vote.valid?
-      flash[:notice] = "Your vote was counted"
-    else
-      flash[:error] = "You can vote only once."
-    end
-    redirect_to :back
+
+    @comment = Comment.find(params[:id])
+    @vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = "Your vote was counted"
+        else
+          flash[:error] = "You can vote only once."
+        end
+        redirect_to :back
+      end
+      format.js
+      end
   end
 end

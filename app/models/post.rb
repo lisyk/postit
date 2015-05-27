58f97f9f1b9e7e64abeec0345cb8ff8c8cf738b1
Lisyk <sergey.liskovoy@gmail.com>
2015-05-26 21:57:15 -0400

    add slugs, fix some bugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,8 @@ class Post < ActiveRecord::Base
   validates :description, presence: true
   validates :url, presence: true, uniqueness: true
 
+  before_save :generate_slug
+
   def total_votes
     up_votes - down_votes
   end
@@ -20,5 +22,13 @@ class Post < ActiveRecord::Base
 
   def down_votes
     self.votes.where(vote: false).size
+  end
+
+  def to_param
+    self.slug
+  end
+
+  def generate_slug
+    self.slug = self.title.gsub(" ", "-").downcase
   end
 end

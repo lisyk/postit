ce4f8da9ab855a9141554c192d11d48eacf2742b
Lisyk <sergey.liskovoy@gmail.com>
2015-05-27 21:10:12 -0400

    add admin role

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ActiveRecord::Base
   validates :password, presence: true, on: :create
 
   sluggable_column :username
+
+  def admin?
+    self.role == "admin"
+  end
+
+  def moderator?
+    self.role == "moderator"
+  end
 end
